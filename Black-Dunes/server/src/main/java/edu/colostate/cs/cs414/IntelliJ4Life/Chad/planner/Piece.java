2afeb0b692916325550a21163c2fb989d0632716
rmwillia <ryanwilliams100@gmail.com>
2018-10-21 15:53:33 -0600

    Created specific piece classes

--- Black-Dunes/server/src/main/java/edu/colostate/cs/cs414/IntelliJ4Life/Chad/planner/Piece.java
+++ Black-Dunes/server/src/main/java/edu/colostate/cs/cs414/IntelliJ4Life/Chad/planner/Piece.java
@@ -1,5 +1,7 @@
 package edu.colostate.cs.cs414.IntelliJ4Life.Chad.planner;
 
+import java.util.ArrayList;
+
 public abstract class Piece {
     enum Color {
         WHITE, BLACK;
@@ -9,6 +11,9 @@ public abstract class Piece {
     private boolean isTaken;
     private Color color;
 
+    /*******************
+     * Accessors
+     ******************/
     public int[] getPosition() {
         return this.position;
     }
@@ -35,23 +40,83 @@ public abstract class Piece {
         this.position = new int[] {-1, -1};
     }
 
-    abstract public int[][] validMoves();
+    /*******************
+     * Public Functions
+     ******************/
+    // Returns a list of valid moves in the form of locations the piece can move to
+    abstract public ArrayList<int[]> validMoves(Piece[][] board);
 
-    abstract public boolean isValid(int[] move);
+    // Conducts the passed in move
+    public void move(int[] move, Piece[][] board) {
+        if (this.validMoves(board).contains(move)) {
+            // Update board
+            board[getPosition()[0]][getPosition()[1]] = null;
+            board[move[0]][move[1]] = this;
 
-    public void move(int[] move) {
-        if (isValid(move)) {
+            // Update position
             this.position = move;
-            // @TODO: Update board with this piece
         }
         else {
             // @TODO: Handle invalid move
         }
     }
 
+    // Removes the piece from the board due to capture
     public void take() {
         this.isTaken = true;
         int[] removedPosition = {-1, -1};
         this.position = removedPosition;
+    }
+
+    /*******************
+     * Helpers
+     ******************/
+    // Returns true or false depending on if the piece is on a caatle wall
+    private boolean onWall(){
+        int[][] walls = {{1, 7}, {1, 8}, {1, 9},
+                         {2, 6}, {3, 6}, {4, 6},
+                         {2, 10}, {3, 10}, {4, 10},
+                         {5, 7}, {5, 8}, {5, 9},
+                         {6, 2}, {6, 3}, {6, 4},
+                         {7, 1}, {8, 1}, {9, 1},
+                         {7, 5}, {8, 5}, {9, 5},
+                         {10, 2}, {10, 3}, {10, 4}};
+
+        for (int i = 0; i < walls.length; i++) {
+            int[] location = walls[i];
+
+            if ((this.getPosition()[0] == location[0]) &&
+                (this.getPosition()[1] == location[1])) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Adds valid moves to the valid moves list and returns whether or not the calling function should keep adding moves
+    protected boolean addMoveToList(int[] move, Piece[][] board, ArrayList<int[]> validMoves){
+        int row = move[0];
+        int col = move[1];
+
+        // Determines whether the calling function should continue adding moves
+        boolean continueAdding = false;
+
+        // Check if the space is empty
+        if (board[row][col] == null) {
+            validMoves.add(move);
+            continueAdding = true;
+        }
+        // Check if the space is occupied by an opponent piece
+        else if (board[row][col].getColor() != this.getColor()) {
+            // Check if piece is capturable: ie, whether one of the two pieces is on a wall
+            if ((this.onWall()) || (board[row][col].onWall())) {
+                validMoves.add(move);
+            }
+        }
+
+        // @Todo: Once board is implemented, add check to ensure move doesn't result in check
+
+        return continueAdding;
     }
 }
---
+++ Black-Dunes/server/src/main/java/edu/colostate/cs/cs414/IntelliJ4Life/Chad/planner/Queen.java
@@ -0,0 +1,127 @@
+package edu.colostate.cs.cs414.IntelliJ4Life.Chad.planner;
+
+import java.util.ArrayList;
+
+public class Queen extends Piece{
+    public Queen(Piece.Color color) {
+        initializePiece(color);
+    }
+
+    @Override
+    public ArrayList<int[]> validMoves(Piece[][] board) {
+        int[] position = this.getPosition();
+        int row = position[0];
+        int col = position[1];
+        ArrayList<int[]> returnList = new ArrayList<int[]>();
+
+        /*
+         * Loop over potential rows
+         */
+
+        // loop over rows less than the current row
+        for (int i = row - 1; i >= 0; i--) {
+            int[] move = {i, col};
+
+            if (!(addMoveToList(move, board, returnList))) {
+                break;
+            }
+        }
+
+        // Loop over rows greater than the current row
+        for (int i = row + 1; i < 12; i++) {
+            int[] move = {i, col};
+
+            if (!(addMoveToList(move, board, returnList))) {
+                break;
+            }
+        }
+
+        /*
+         * Loop over potential columns
+         */
+
+        // loop over columns less than the current column
+        for (int i = col - 1; i >= 0; i--) {
+            int[] move = {row, i};
+
+            if (!(addMoveToList(move, board, returnList))) {
+                break;
+            }
+        }
+
+        // loop over columns greater than the current column
+        for (int i = col + 1; i < 12; i--) {
+            int[] move = {row, i};
+
+            if (!(addMoveToList(move, board, returnList))) {
+                break;
+            }
+        }
+
+        /*
+         * Loop over diagonals
+         */
+
+        // Up and left
+        int i = row - 1;
+        int j = col - 1;
+
+        while (i >= 0 && j >= 0) {
+            int[] move = {i, j};
+
+            if (!(addMoveToList(move, board, returnList))) {
+                break;
+            }
+
+            i--;
+            j--;
+        }
+
+        // Up and right
+        i = row - 1;
+        j = col + 1;
+
+        while (i >= 0 && j < 12) {
+            int[] move = {i, j};
+
+            if (!(addMoveToList(move, board, returnList))) {
+                break;
+            }
+
+            i--;
+            j++;
+        }
+
+        // Down and left
+        i = row + 1;
+        j = col - 1;
+
+        while (i < 12 && j >= 0) {
+            int[] move = {i, j};
+
+            if (!(addMoveToList(move, board, returnList))) {
+                break;
+            }
+
+            i++;
+            j--;
+        }
+
+        // Down and right
+        i = row + 1;
+        j = col + 1;
+
+        while (i < 12 && j < 12) {
+            int[] move = {i, j};
+
+            if (!(addMoveToList(move, board, returnList))) {
+                break;
+            }
+
+            i++;
+            j++;
+        }
+
+        return returnList;
+    }
+}
