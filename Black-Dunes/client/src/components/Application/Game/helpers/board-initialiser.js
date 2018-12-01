032ea5d871fb6ce059699f4ba91c1883aac661da
Sandeep Chundru <chundrus@rams.colostate.edu>
2018-12-01 09:49:26 -0700

    added initial pieces to board

--- Black-Dunes/client/src/components/Application/Game/helpers/board-initialiser.js
+++ Black-Dunes/client/src/components/Application/Game/helpers/board-initialiser.js
@@ -6,34 +6,38 @@ import Queen from '../pieces/queen.js';
 import Rook from '../pieces/rook.js';
 
 export default function initialiseChessBoard(){
-  const squares = Array(64).fill(null);
+  const squares = Array(144).fill(null);
 
-  // TODO SANDEEP Make initial board
+  // 1 = White
+  // 2 = Black
 
-  for(let i = 8; i < 16; i++){
-    squares[i] = new Pawn(2);
-    squares[i+40] = new Pawn(1);
-  }
-  squares[0] = new Rook(2);
-  squares[7] = new Rook(2);
-  squares[56] = new Rook(1);
-  squares[63] = new Rook(1);
+  // Black Pieces
+    // Top row
+  squares[33] = new Rook(2)
+  squares[32] = new Rook(2)
+  squares[31] = new Rook(2)
+    // Middle row
+  squares[45] = new Rook(2)
+  squares[44] = new King(2)
+  squares[43] = new Rook(2)
+    // Bottom row
+  squares[57] = new Rook(2)
+  squares[56] = new Rook(2)
+  squares[55] = new Rook(2)
 
-  squares[1] = new Knight(2);
-  squares[6] = new Knight(2);
-  squares[57] = new Knight(1);
-  squares[62] = new Knight(1);
-
-  squares[2] = new Bishop(2);
-  squares[5] = new Bishop(2);
-  squares[58] = new Bishop(1);
-  squares[61] = new Bishop(1);
-
-  squares[3] = new Queen(2);
-  squares[4] = new King(2);
-
-  squares[59] = new Queen(1);
-  squares[60] = new King(1);
+  // White Pieces
+    // Top row
+  squares[110] = new Rook(1)
+  squares[111] = new Rook(1)
+  squares[112] = new Rook(1)
+    // Middle row
+  squares[98] = new Rook(1)
+  squares[99] = new King(1)
+  squares[100] = new Rook(1)
+    // Bottom row
+  squares[86] = new Rook(1)
+  squares[87] = new Rook(1)
+  squares[88] = new Rook(1)
 
   return squares;
 }
