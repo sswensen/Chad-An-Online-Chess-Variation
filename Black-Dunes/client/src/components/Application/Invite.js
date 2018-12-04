9ba59a7a619299f3e9711cb60b0ff35b13d5663f
chrishaynes21 <chris.haynes2112@gmail.com>
2018-12-03 18:49:57 -0700

    Clean up invite

--- Black-Dunes/client/src/components/Application/Invite.js
+++ Black-Dunes/client/src/components/Application/Invite.js
@@ -1,13 +1,8 @@
 import React, {Component} from 'react'
 import Select from 'react-select'
 import {Button, Card, CardBody, Col, Container, Row} from 'reactstrap'
-import {request} from "../../api/api";
+import {request} from '../../api/api';
 
-/* Options allows the user to change the parameters for planning
- * and rendering the trip map and itinerary.
- * The options reside in the parent object so they may be shared with the Trip object.
- * Allows the user to set the options used by the application via a set of buttons.
- */
 class Invite extends Component {
     constructor(props) {
         super(props);
