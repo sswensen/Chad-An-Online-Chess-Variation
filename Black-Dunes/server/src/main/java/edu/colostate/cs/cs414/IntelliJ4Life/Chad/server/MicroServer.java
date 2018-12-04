00f1058553c5dfd1f89d6c986ddb1f26b3bf1ff6
ColtonLarson <cwlarson@rams.colostate.edu>
2018-12-03 18:07:31 -0700

    Valid moves is fixed!

--- Black-Dunes/server/src/main/java/edu/colostate/cs/cs414/IntelliJ4Life/Chad/server/MicroServer.java
+++ Black-Dunes/server/src/main/java/edu/colostate/cs/cs414/IntelliJ4Life/Chad/server/MicroServer.java
@@ -162,7 +162,6 @@ public class MicroServer {
     response.type("text/plain");
     response.header("Access-Control-Allow-Origin", "*");
 
-    System.out.println();
     LoginSession lSesh = new LoginSession(request);
     Database db = new Database(lSesh.getAuthUser());
     db.getCurrentGamesFromDatabase();
@@ -186,7 +185,6 @@ public class MicroServer {
     response.type("text/plain");
     response.header("Access-Control-Allow-Origin", "*");
 
-    System.out.println();
     return new RegisterSession(request).getUserID(); // Send back user id, THIS IS INSECURE
   }
 
@@ -292,7 +290,6 @@ public class MicroServer {
     response.type("text/plain");
     response.header("Access-Control-Allow-Origin", "*");
 
-    System.out.println("getBoard");
     return new GetBoardSession(request, activeGames).getBoard();
   }
 }
